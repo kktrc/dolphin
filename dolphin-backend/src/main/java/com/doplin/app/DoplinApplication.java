1f0da4063179c707c4ebc847797153b0a418e8b4
moshenglei <moshenglei@maoyan.com>
2021-09-06 11:40:37 +0800

    init

---
+++ dolphin-backend/src/main/java/com/doplin/app/DoplinApplication.java
@@ -0,0 +1,13 @@
+package com.doplin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoplinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoplinApplication.class, args);
+	}
+
+}
